665cb6342a4c34a241b514867d9af28708d8f503
Whaiy <Whaiy@github.com>
2022-09-03 20:12:55 +0800

    fix repositry tree

--- hardware_stuff/orange_pi/wifi_test.sh
+++
@@ -1,7 +0,0 @@
-nmcli connection down target_router_ssid;
-nmcli connection down debug_router_ssid;
-
-nmcli connection delete target_router_ssid;
-nmcli connection delete debug_router_ssid;
-
-reboot;
