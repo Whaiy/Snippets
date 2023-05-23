548dd7ce2aa0617ce284f1646d4c295acbddc4df
Whaiy <50110914+Whaiy@users.noreply.github.com>
2023-05-23 19:05:41 +0800

    Update wifi_check.sh

--- hardware_sh/orangepi/wifi_check.sh
+++ hardware_sh/orangepi/wifi_check.sh
@@ -3,9 +3,10 @@
 # def wifi_check
 
 wifi_check_fn(){
-    /var/pub_sh/wifi_init.sh > /var/pub_sh/log/wifi_init.log
+        date > /var/pub_sh/log/wifi_check.log;
+        echo "wifi checked !" >> /var/pub_sh/log/wifi_check.log
+       /var/pub_sh/wifi_init.sh > /var/pub_sh/log/wifi_init.log
 }
 
 # runner
-
-wifi_check_fn &
+wifi_check_fn
